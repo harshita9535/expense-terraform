04f7dd0ec6699532833c8e058c4cf317b1929944
Harshita <harshitabhadimane02051998@gmail.com>
2024-11-17 12:42:26 +0530

    modules directory correction

--- main.tf
+++ main.tf
@@ -1,16 +1,26 @@
 module "frontend" {
-  source = "./modules/app"
+  source        = "./modules/app"
   instance_type = var.instance_type
-  component = "frontend"
-  ssh_user = var.ssh_user
-  ssh_pass = var.ssh_pass
-  env = var.env
+  component     = "frontend"
+  ssh_user      = var.ssh_user
+  ssh_pass      = var.ssh_pass
+  env           = var.env
 }
 
-#module "backend" {
-#  source = "./modules/app"
-#}
+module "backend" {
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "backend"
+  ssh_user      = var.ssh_user
+  ssh_pass      = var.ssh_pass
+  env           = var.env
+}
 
-#module "mysql" {
-#  source = "./modules/app"
-#}
+module "mysql" {
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "mysql"
+  ssh_user      = var.ssh_user
+  ssh_pass      = var.ssh_pass
+  env           = var.env
+}
